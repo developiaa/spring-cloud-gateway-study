ace5b73f10cda146c2b66ca95a37c94ea58da38b
developiaa <developiaa@gmail.com>
2022-08-18 21:07:55 +0900

    feat: Add AuthorizationHeaderFilter with jwt

---
+++ src/main/java/study/developia/gateway/filter/AuthorizationHeaderFilter.java
@@ -0,0 +1,80 @@
+package study.developia.gateway.filter;
+
+import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpHeaders;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+
+    private final Environment environment;
+
+    //login > token > users (with token) > header(include token)
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            // 헤더에 토큰 정보를 포함하고 있는지 체크
+            if (!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                return onError(exchange, "no authorization header", HttpStatus.UNAUTHORIZED);
+            }
+
+            String authorizationHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+            String jwt = authorizationHeader.replace("Bearer", "");
+
+            if (!isJwtValid(jwt)) {
+                return onError(exchange, "JWT is not valid", HttpStatus.UNAUTHORIZED);
+            }
+
+            return chain.filter(exchange);
+        });
+    }
+
+    private boolean isJwtValid(String jwt) {
+        boolean returnValue = true;
+        String subject = null;
+
+        try {
+            subject = Jwts.parser().setSigningKey(environment.getProperty("token.secret"))
+                    .parseClaimsJws(jwt).getBody()
+                    .getSubject();
+        } catch (Exception exception) {
+            returnValue = false;
+        }
+
+        if (subject == null || subject.isEmpty()) {
+            returnValue = false;
+        }
+
+
+        return returnValue;
+    }
+
+    // Mono(단일값), Flux(복수값) -> Spring WebFlux
+    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
+        log.error(err);
+        return response.setComplete();
+    }
+
+    public static class Config {
+
+    }
+
+
+}
